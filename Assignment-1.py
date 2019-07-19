2bf1494df5555cd2aa19deb84b93217be0486626
kmadane <32280970+kmadane@users.noreply.github.com>
2019-07-19 16:32:25 -0700

    Add files via upload

---
+++ Assignment-1.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#1
+print("Hello My name is 'Krishna'.I love coding")
+print("This is my first program")
+
+
+# In[2]:
+
+
+x=input("Number: ")
+print(x)
+y=input("Number: ")
+print(y)
+z=input("Number: ")
+print(z)
+type(x)
+type(y)
+type(z)
+
+
+# In[3]:
+
+
+#2
+print("Hello! My name is XYZ")
+print("Hello I am a candidate")
+print("234.56")
+print("34")
+print("a+3j")
+
+
+# In[4]:
+
+
+#3
+x=10+20*(45+67.0)
+x
+type(x)
+x=(True and False)or False
+x
+type(x)
+x=(True or True)and (not False and True)
+x
+type(x)
+x=(3>89)or(34>32)
+x
+type(x)
+x=not True and False
+x
+
+
+# In[5]:
+
+
+y=int(input("Enter a number: "))
+if x%2==0 and x%5==0:
+    print("Hurray it is what I am looking for")
+else:
+    print("Wrong input")
+
+
+# In[6]:
+
+
+x=int(input("Enter a number: "))
+if x in range(10,51):
+    print("Yes I am in the range")
+else:
+    print("Oops")
+
+
+# In[ ]:
+
+
+
+
